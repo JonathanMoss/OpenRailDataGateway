cb3e46a788ca374005e644a9dffff7bf1e90646a
Jonathan Moss <joth.moss@googlemail.com>
2023-12-16 14:59:35 +0000

    ...

--- gateway/nrod/nrod_connection.py
+++ gateway/nrod/nrod_connection.py
@@ -464,7 +464,7 @@ class NRODConnection(pydantic.BaseModel):
 
     topics: List[str] = pydantic.Field(
         title='A list of topics in which to subscribe to',
-        default=[TD_TOPIC, MVT_TOPIC]
+        default=[S_CLASS]
     )
 
     # topics: List[str] = pydantic.Field(
